b9690c8b81e5c269853bbb55f88226b496b30526
Jim Zenn <jimzenn0@gmail.com>
2019-07-10 01:39:46 -0700

    save

--- pj1a/solution/lab1a.c
+++ pj1a/solution/lab1a.c
@@ -5,8 +5,11 @@
  * Distributed under terms of the MIT license.
  */
 
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -16,6 +19,11 @@
 
 #define EOT 0x04  // ctrl + D, i.e. End-of-Transmission character (EOT)
 #define BUF_SIZE 256
+
+/* For long options that have no equivalent short option, use a
+  non-character as a pseudo short option, starting with CHAR_MAX + 1.
+  Here, we use CHAR_MAX = 255 */
+#define SHELL_SHORT_OPTION 256
 
 char const program_name[] = PROGRAM_NAME;
 
@@ -52,8 +60,14 @@ With no --shell option, it reads (writes) standard input (output).\n\
 
 int main(int argc, char *argv[]) {
   struct termios orig_termios_p, new_termios_p;
-  char c;
-  char buf[BUF_SIZE];
+  bool shell_set = false;
+  char shell_program[64];
+
+  /* options descriptor */
+  static struct option const long_opts[] = {
+      {"shell", required_argument, NULL, SHELL_SHORT_OPTION},
+      {0, 0, 0, 0},
+  };
 
   tcgetattr(STDIN_FILENO, &orig_termios_p);  // keey a backup
 
@@ -63,21 +77,65 @@ int main(int argc, char *argv[]) {
   new_termios_p.c_lflag = 0;       // no processing
   tcsetattr(STDIN_FILENO, TCSANOW, &new_termios_p);
 
-  while (1) {
-    read(STDIN_FILENO, buf, sizeof(buf));
-    for (int i = 0; i < BUF_SIZE; i++) {
-      c = buf[i];
-      if (c == EOT) {
-        write(STDOUT_FILENO, EOT_REPR, sizeof(EOT_REPR));
-        tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios_p);
-        exit(EXIT_SUCCESS);
-      }
-      // restore terminal, exit.
-      else if (c == '\r' || c == '\n')
-        write(STDOUT_FILENO, CRLF, sizeof(CRLF));
-      else
-        write(1, &c, sizeof(char));
+  int optc;
+  /* option parsing */
+  while ((optc = getopt_long(argc, argv, ":", long_opts, NULL)) != -1) {
+    switch (optc) {
+      case SHELL_SHORT_OPTION:
+        /* use the given program as the shell */
+        shell_set = true;
+        strcpy(optarg, shell_program);
+        break;
+      default:
+        usage(EXIT_FAILURE);
     }
+  }
+
+  int proc_id = -2;
+
+  if (shell_set) proc_id = fork();
+  // TODO: check errno
+
+  int pipe_fd[2] = {STDIN_FILENO, STDOUT_FILENO};
+  int *in_fd = &pipe_fd[0];
+  int *out_fd = &pipe_fd[1];
+
+  if (proc_id > 0) {
+    // successfully created a child process, create pipe
+    if (pipe(pipe_fd)) {
+      // TODO: check errno
+    }
+  }
+
+  if (proc_id != 0) {
+    char buf[BUF_SIZE];
+    char c;
+    // parent process
+    while (true) {
+      read(STDIN_FILENO, buf, sizeof(buf));
+      // TODO: check errno
+      for (int i = 0; i < BUF_SIZE; i++) {
+        c = buf[i];
+        if (c == EOT) {
+          write(*in_fd, EOT_REPR, sizeof(EOT_REPR));
+          // TODO: check errno
+          tcsetattr(*out_fd, TCSANOW, &orig_termios_p);
+          exit(EXIT_SUCCESS);
+        }
+        // restore terminal, exit.
+        else if (c == '\r' || c == '\n')
+          write(*out_fd, CRLF, sizeof(CRLF));
+        else
+          write(1, &c, sizeof(char));
+      }
+    }
+  } else {
+    // child process
+    close(to_shell[1]);
+    close(0);
+    dup(to_shell[0]);
+    close(to_shell[0]);
+    execlp(“lab0”, “lab0”, NULL);
   }
 
   return 0;
